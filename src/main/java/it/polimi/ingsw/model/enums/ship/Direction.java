c2551aebd8f2c99f771857c44cad7ca50834c529
diego-zanca <diego.zanca@mail.polimi.it>
2025-03-11 16:00:59 +0100

    docs(model): added JavaDoc comments to the model

--- src/main/java/it/polimi/ingsw/model/enums/ship/Direction.java
+++ src/main/java/it/polimi/ingsw/model/enums/ship/Direction.java
@@ -1,14 +1,40 @@
 package it.polimi.ingsw.model.enums.ship;
 
 public enum Direction {
-    UP, DOWN, LEFT, RIGHT;
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT;
 
+    /**
+     * Returns the number of 90° rotations needed to get from the initial direction (from) to the final direction (to).
+     * @param from The initial direction
+     * @param to The final direction
+     * @return The number of steps needed
+     */
     public int getRotationSteps(Direction from, Direction to) {return 0;}
-    public Direction getOpposite() {return null;}
 
-    // rotates direction 90° clockwise and returns it
-    public Direction rotateClockwise() {return null;}
+    /**
+     * Gets the opposite direction to the given one.
+     * @return The opposite direction
+     */
+    public Direction getOpposite() {
+        return null;
+    }
 
-    // rotates direction 90° counterclockwise and returns it
-    public Direction rotateCounterClockwise() {return null;}
+    /**
+     * Rotates direction 90° clockwise and returns the resulting direction.
+     * @return The direction after the 90° clockwise rotation
+     */
+    public Direction rotateClockwise() {
+        return null;
+    }
+
+    /**
+     * Rotates direction 90° counterclockwise and returns the resulting direction.
+     * @return The direction after the 90° counterclockwise rotation
+     */
+    public Direction rotateCounterClockwise() {
+        return null;
+    }
 }
