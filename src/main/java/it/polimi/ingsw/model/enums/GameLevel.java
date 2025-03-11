2aa76121ae90b079db39d3c434b7fabbe1cd8117
diego-zanca <diego.zanca@mail.polimi.it>
2025-03-11 16:58:22 +0100

    Implemented missing enums

--- src/main/java/it/polimi/ingsw/model/enums/GameLevel.java
+++ src/main/java/it/polimi/ingsw/model/enums/GameLevel.java
@@ -1,4 +1,64 @@
 package it.polimi.ingsw.model.enums;
 
+import it.polimi.ingsw.model.enums.adventure.CardLevel;
+import java.util.Map;
+
 public enum GameLevel {
+    // sistemare valori
+    TEST_FLIGHT(0, 0, 8, null),
+
+    // sistema valori
+    LEVEL_II(0, 0, 0, null);
+
+    private int duration;
+    private int predictablePileCount;
+    private int cardPerPile;
+    private CardLevel primaryCardLevel;
+
+    private GameLevel(int duration, int predictablePileCount, int cardPerPile, CardLevel primaryCardLevel) {
+        this.duration = duration;
+        this.predictablePileCount = predictablePileCount;
+        this.cardPerPile = cardPerPile;
+        this.primaryCardLevel = primaryCardLevel;
+    }
+
+    /**
+     * Gets the duration of the game level (hourglass).
+     * @return The duration of the game level (hourglass).
+     */
+    public int getDuration() {
+        return duration;
+    }
+
+    /**
+     * Gets the total number of predictable piles.
+     * @return The total number of predictable piles
+     */
+    public int getPredictablePileCount() {
+        return predictablePileCount;
+    }
+
+    /**
+     * Get the number of cards per pile.
+     * @return The number of cards per pile
+     */
+    public int getCardPerPile() {
+        return cardPerPile;
+    }
+
+    /**
+     * Gets the level of the primary card.
+     * @return The level of the primary card
+     */
+    public CardLevel getPrimaryCardLevel() {
+        return primaryCardLevel;
+    }
+
+    /**
+     * Returns the card distribution of the piles (how many cards for each game level).
+     * @return A map that associates to each card level the corresponding number of cards per pile
+     */
+    public Map<CardLevel, Integer> getCardDistributionForPile() {
+        return null;
+    }
 }
--- src/main/java/it/polimi/ingsw/model/enums/GamePhase.java
+++ src/main/java/it/polimi/ingsw/model/enums/GamePhase.java
@@ -1,4 +1,8 @@
 package it.polimi.ingsw.model.enums;
 
 public enum GamePhase {
+    SETUP,
+    BUILDING,
+    FLIGHT,
+    END;
 }
